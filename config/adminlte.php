09073538e86284fa810230dea8f86038f5cb327f
skuze <skuzevanov@gmail.com>
2026-06-24 14:38:33 +0500

    Поправил название и лого.

--- config/adminlte.php
+++ config/adminlte.php
@@ -22,7 +22,7 @@ return [
     |
     */
 
-    'title' => 'AdminLTE 3',
+    'title' => 'RFPMW',
     'title_prefix' => '',
     'title_postfix' => '',
 
@@ -71,12 +71,12 @@ return [
     |
     */
 
-    'logo' => '<b>CRM</b>',
-    'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
+    'logo' => '<b>RFPMW</b>',
+    'logo_img' => 'img/logo.jpg',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
-    'logo_img_alt' => 'Admin Logo',
+    'logo_img_alt' => 'RFPMW',
 
     /*
     |--------------------------------------------------------------------------
