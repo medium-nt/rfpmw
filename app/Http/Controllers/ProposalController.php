23964748a6cff3e2605b2b28c7839f71ef12acf2
skuze <skuzevanov@gmail.com>
2026-07-06 13:03:34 +0500

    Добавил фильтры даты на страницы.

--- app/Http/Controllers/ProposalController.php
+++ app/Http/Controllers/ProposalController.php
@@ -22,8 +22,11 @@ class ProposalController extends Controller
             ->when(auth()->user()->isManager(), function ($q): void {
                 $q->whereHas('employedPerson.contractor', fn ($qq) => $qq->where('user_id', auth()->id()));
             })
+            ->when(request('from'), fn ($q) => $q->where('date', '>=', request('from')))
+            ->when(request('to'), fn ($q) => $q->where('date', '<=', request('to')))
             ->orderBy('id')
-            ->paginate(10);
+            ->paginate(10)
+            ->appends(['from' => request('from'), 'to' => request('to')]);
 
         return view('proposals.index', compact('proposals'));
     }
---
+++ public/js/page-query-param.js
@@ -0,0 +1,19 @@
+function updatePageWithQueryParam(selectElement) {
+    const paramName = selectElement.name;
+    const paramValue = selectElement.value;
+
+    const urlParams = new URLSearchParams(window.location.search);
+
+    // Проверяем, есть ли уже параметр с таким именем, и удаляем его перед установкой нового значения
+    urlParams.delete(paramName);
+
+    // Добавляем новый параметр
+    if (paramValue !== '' && paramValue !== 'all') {
+        urlParams.append(paramName, paramValue);
+    }
+
+    // Удаляем scan_order_id при выборе фильтров
+    urlParams.delete('scan_order_id');
+
+    window.location.assign(`${window.location.origin}${window.location.pathname}?${urlParams}`);
+}
